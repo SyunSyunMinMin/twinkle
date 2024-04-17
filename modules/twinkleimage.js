08135cf5f9f1e353f9f3bb4d47e0059c78f52ce9
SyunSyunMinMin <109850704+SyunSyunMinMin@users.noreply.github.com>
2024-04-17 23:48:18 +0900

    日本語化

--- modules/twinkleimage.js
+++ modules/twinkleimage.js
@@ -14,28 +14,28 @@
 
 Twinkle.image = function twinkleimage() {
 	if (mw.config.get('wgNamespaceNumber') === 6 && mw.config.get('wgArticleId') && !document.getElementById('mw-sharedupload') && !Morebits.isPageRedirect()) {
-		Twinkle.addPortletLink(Twinkle.image.callback, 'DI', 'tw-di', 'Nominate file for delayed speedy deletion');
+		Twinkle.addPortletLink(Twinkle.image.callback, 'DI', 'tw-di', '保留期間がある即時削除対象のファイルを指定');
 	}
 };
 
 Twinkle.image.callback = function twinkleimageCallback() {
 	var Window = new Morebits.simpleWindow(600, 330);
-	Window.setTitle('File for dated speedy deletion');
+	Window.setTitle('日付入り即時削除依頼');
 	Window.setScriptName('Twinkle');
-	Window.addFooterLink('Speedy deletion policy', 'WP:CSD#Files');
-	Window.addFooterLink('Image prefs', 'WP:TW/PREF#image');
-	Window.addFooterLink('Twinkle help', 'WP:TW/DOC#image');
-	Window.addFooterLink('Give feedback', 'WT:TW');
+	Window.addFooterLink('即時削除の方針', 'WP:CSD#F');
+	Window.addFooterLink('Image設定', 'User:Syunsyunminmin/Twinkle/Preferences#image');
+	Window.addFooterLink('Twinkle help', ':en:WP:TW/DOC#image');
+	Window.addFooterLink('フィードバック', 'User talk:Syunsyunminmin/Twinkle');
 
 	var form = new Morebits.quickForm(Twinkle.image.callback.evaluate);
 	form.append({
 		type: 'checkbox',
 		list: [
 			{
-				label: 'Notify original uploader',
+				label: 'アップロード者に通知',
 				value: 'notify',
 				name: 'notify',
-				tooltip: "Uncheck this if you are planning to make multiple nominations from the same user, and don't want to overload their talk page with too many notifications.",
+				tooltip: '同じ利用者から複数の指定を行う予定で、その利用者の会話ページに多くの通知を送りたくない場合は、このチェックを外してください。',
 				checked: Twinkle.getPref('notifyUserOnDeli')
 			}
 		]
@@ -43,60 +43,24 @@ Twinkle.image.callback = function twinkleimageCallback() {
 	);
 	var field = form.append({
 		type: 'field',
-		label: 'Type of action wanted'
+		label: '行おうとしている操作'
 	});
 	field.append({
 		type: 'radio',
 		name: 'type',
-		event: Twinkle.image.callback.choice,
 		list: [
 			{
-				label: 'No source (CSD F4)',
+				label: '出典情報なし (CSD F5)',
 				value: 'no source',
 				checked: true,
-				tooltip: 'Image or media has no source information'
+				tooltip: '出典についての情報がないファイル'
 			},
 			{
-				label: 'No license (CSD F4)',
+				label: 'ライセンス情報なし (CSD F5)',
 				value: 'no license',
-				tooltip: 'Image or media does not have information on its copyright status'
-			},
-			{
-				label: 'No source and no license (CSD F4)',
-				value: 'no source no license',
-				tooltip: 'Image or media has neither information on source nor its copyright status'
-			},
-			{
-				label: 'Orphaned fair use (CSD F5)',
-				value: 'orphaned fair use',
-				tooltip: 'Image or media is unlicensed for use on Wikipedia and allowed only under a claim of fair use per Wikipedia:Non-free content, but it is not used in any articles'
-			},
-			{
-				label: 'No fair use rationale (CSD F6)',
-				value: 'no fair use rationale',
-				tooltip: 'Image or media is claimed to be used under Wikipedia\'s fair use policy but has no explanation as to why it is permitted under the policy'
-			},
-			{
-				label: 'Disputed fair use rationale (CSD F7)',
-				value: 'disputed fair use rationale',
-				tooltip: 'Image or media has a fair use rationale that is disputed or invalid, such as a {{Non-free logo}} tag on a photograph of a mascot'
-			},
-			{
-				label: 'Replaceable fair use (CSD F7)',
-				value: 'replaceable fair use',
-				tooltip: 'Image or media may fail Wikipedia\'s first non-free content criterion ([[WP:NFCC#1]]) in that it illustrates a subject for which a free image might reasonably be found or created that adequately provides the same information'
-			},
-			{
-				label: 'No evidence of permission (CSD F11)',
-				value: 'no permission',
-				tooltip: 'Image or media does not have proof that the author agreed to licence the file'
+				tooltip: '著作権状態についての情報がないファイル'
 			}
 		]
-	});
-	form.append({
-		type: 'div',
-		label: 'Work area',
-		name: 'work_area'
 	});
 	form.append({ type: 'submit' });
 
@@ -110,76 +74,6 @@ Twinkle.image.callback = function twinkleimageCallback() {
 	result.type[0].dispatchEvent(evt);
 };
 
-Twinkle.image.callback.choice = function twinkleimageCallbackChoose(event) {
-	var value = event.target.values;
-	var root = event.target.form;
-	var work_area = new Morebits.quickForm.element({
-		type: 'div',
-		name: 'work_area'
-	});
-
-	switch (value) {
-		case 'no source no license':
-		case 'no source':
-			work_area.append({
-				type: 'checkbox',
-				list: [
-					{
-						label: 'Non-free',
-						name: 'non_free',
-						tooltip: 'File is licensed under a fair use claim'
-					}
-				]
-			});
-		/* falls through */
-		case 'no license':
-			work_area.append({
-				type: 'checkbox',
-				list: [
-					{
-						name: 'derivative',
-						label: 'Derivative work which lacks a source for incorporated works',
-						tooltip: 'File is a derivative of one or more other works whose source is not specified'
-					}
-				]
-			});
-			break;
-		case 'no permission':
-			work_area.append({
-				type: 'input',
-				name: 'source',
-				label: 'Source:'
-			});
-			break;
-		case 'disputed fair use rationale':
-			work_area.append({
-				type: 'textarea',
-				name: 'reason',
-				label: 'Concern:'
-			});
-			break;
-		case 'orphaned fair use':
-			work_area.append({
-				type: 'input',
-				name: 'replacement',
-				label: 'Replacement:',
-				tooltip: 'Optional file that replaces this one.  The "File:" prefix is optional.'
-			});
-			break;
-		case 'replaceable fair use':
-			work_area.append({
-				type: 'textarea',
-				name: 'reason',
-				label: 'Reason:'
-			});
-			break;
-		default:
-			break;
-	}
-
-	root.replaceChild(work_area.render(), $(root).find('div[name="work_area"]')[0]);
-};
-
 Twinkle.image.callback.evaluate = function twinkleimageCallbackEvaluate(event) {
 
 	var input = Morebits.quickForm.getInputData(event.target);
@@ -187,35 +81,26 @@ Twinkle.image.callback.evaluate = function twinkleimageCallbackEvaluate(event) {
 		input.replacement = (new RegExp('^' + Morebits.namespaceRegex(6) + ':', 'i').test(input.replacement) ? '' : 'File:') + input.replacement;
 	}
 
-	var csdcrit;
+	var csdcrit, notifytemplatename;
 	switch (input.type) {
-		case 'no source no license':
 		case 'no source':
-		case 'no license':
-			csdcrit = 'F4';
-			break;
-		case 'orphaned fair use':
+			notifytemplatename += 'Image source';
 			csdcrit = 'F5';
 			break;
-		case 'no fair use rationale':
-			csdcrit = 'F6';
-			break;
-		case 'disputed fair use rationale':
-		case 'replaceable fair use':
-			csdcrit = 'F7';
-			break;
-		case 'no permission':
-			csdcrit = 'F11';
+		case 'no license':
+			notifytemplatename += 'Image copyright';
+			csdcrit = 'F5';
 			break;
 		default:
-			throw new Error('Twinkle.image.callback.evaluate: unknown criterion');
+			throw new Error('Twinkle.image.callback.evaluate: 不明な基準');
 	}
 
 	var lognomination = Twinkle.getPref('logSpeedyNominations') && Twinkle.getPref('noLogOnSpeedyNomination').indexOf(csdcrit.toLowerCase()) === -1;
-	var templatename = input.derivative ? 'dw ' + input.type : input.type;
+	var templatename = input.type;
 
 	var params = $.extend({
 		templatename: templatename,
+		notifytemplatename: notifytemplatename,
 		normalized: csdcrit,
 		lognomination: lognomination
 	}, input);
@@ -224,10 +109,10 @@ Twinkle.image.callback.evaluate = function twinkleimageCallbackEvaluate(event) {
 	Morebits.status.init(event.target);
 
 	Morebits.wiki.actionCompleted.redirect = mw.config.get('wgPageName');
-	Morebits.wiki.actionCompleted.notice = 'Tagging complete';
+	Morebits.wiki.actionCompleted.notice = 'タグ付け完了';
 
 	// Tagging image
-	var wikipedia_page = new Morebits.wiki.page(mw.config.get('wgPageName'), 'Tagging file with deletion tag');
+	var wikipedia_page = new Morebits.wiki.page(mw.config.get('wgPageName'), 'ファイルに画像削除タグを付ける');
 	wikipedia_page.setCallbackParameters(params);
 	wikipedia_page.load(Twinkle.image.callbacks.taggingImage);
 
@@ -241,8 +126,8 @@ Twinkle.image.callback.evaluate = function twinkleimageCallbackEvaluate(event) {
 		}
 		// No auto-notification, display what was going to be added.
 		var noteData = document.createElement('pre');
-		noteData.appendChild(document.createTextNode('{{subst:di-' + templatename + '-notice|1=' + mw.config.get('wgTitle') + '}} ~~~~'));
-		Morebits.status.info('Notification', [ 'Following/similar data should be posted to the original uploader:', document.createElement('br'), noteData ]);
+		noteData.appendChild(document.createTextNode('{{subst:' + notifytemplatename + '|1=' + mw.config.get('wgTitle') + '}} --~~~~'));
+		Morebits.status.info('通知', [ '以下のようなデータは、元のアップロード者に投稿されるべきです:', document.createElement('br'), noteData ]);
 	}
 };
 
@@ -254,31 +139,14 @@ Twinkle.image.callbacks = {
 		// remove "move to Commons" tag - deletion-tagged files cannot be moved to Commons
 		text = text.replace(/\{\{(mtc|(copy |move )?to ?commons|move to wikimedia commons|copy to wikimedia commons)[^}]*\}\}/gi, '');
 
-		var tag = '{{di-' + params.templatename + '|date={{subst:#time:j F Y}}';
+		var tag = '{{subst:' + params.templatename + '}}\n';
 		switch (params.type) {
-			case 'no source no license':
-			case 'no source':
-				tag += params.non_free ? '|non-free=yes' : '';
-				break;
-			case 'no permission':
-				tag += params.source ? '|source=' + params.source : '';
-				break;
-			case 'disputed fair use rationale':
-				tag += params.reason ? '|concern=' + params.reason : '';
-				break;
-			case 'orphaned fair use':
-				tag += params.replacement ? '|replacement=' + params.replacement : '';
-				break;
-			case 'replaceable fair use':
-				tag += params.reason ? '|1=' + params.reason : '';
-				break;
 			default:
 				break;  // doesn't matter
 		}
-		tag += '|help=off}}\n';
 
 		pageobj.setPageText(tag + text);
-		pageobj.setEditSummary('This file is up for deletion, per [[WP:CSD#' + params.normalized + '|CSD ' + params.normalized + ']] (' + params.type + ').');
+		pageobj.setEditSummary('このファイルは[[WP:CSD#' + params.normalized + '|CSD ' + params.normalized + ']]に従い削除される予定です (' + params.type + ')');
 		pageobj.setChangeTags(Twinkle.changeTags);
 		pageobj.setWatchlist(Twinkle.getPref('deliWatchPage'));
 		pageobj.setCreateOption('nocreate');
@@ -290,17 +158,12 @@ Twinkle.image.callbacks = {
 
 		// disallow warning yourself
 		if (initialContrib === mw.config.get('wgUserName')) {
-			pageobj.getStatusElement().warn('You (' + initialContrib + ') created this page; skipping user notification');
+			pageobj.getStatusElement().warn('あなた (' + initialContrib + ') がこのページを作成しました。通知をスキップします。');
 		} else {
-			var usertalkpage = new Morebits.wiki.page('User talk:' + initialContrib, 'Notifying initial contributor (' + initialContrib + ')');
-			var notifytext = '\n{{subst:di-' + params.templatename + '-notice|1=' + mw.config.get('wgTitle');
-			if (params.type === 'no permission') {
-				notifytext += params.source ? '|source=' + params.source : '';
-			}
-			notifytext += '}} ~~~~';
+			var usertalkpage = new Morebits.wiki.page('User talk:' + initialContrib, 'このページの立項者 (' + initialContrib + ') へ通知');
+			var notifytext = '\n{{subst:' + params.notifytemplatename + '|1=' + mw.config.get('wgTitle') + '}} --~~~~';
 			usertalkpage.setAppendText(notifytext);
-			usertalkpage.setEditSummary('Notification: tagging for deletion of [[:' + Morebits.pageNameNorm + ']].');
-			usertalkpage.setChangeTags(Twinkle.changeTags);
+			usertalkpage.setEditSummary('通知: [[:' + Morebits.pageNameNorm + ']] の削除へのタグ付け' + Twinkle.summaryAd);
 			usertalkpage.setCreateOption('recreate');
 			usertalkpage.setWatchlist(Twinkle.getPref('deliWatchUser'));
 			usertalkpage.setFollowRedirect(true, false);
@@ -315,24 +178,23 @@ Twinkle.image.callbacks = {
 	addToLog: function(params, initialContrib) {
 		var usl = new Morebits.userspaceLogger(Twinkle.getPref('speedyLogPageName'));
 		usl.initialText =
-			"This is a log of all [[WP:CSD|speedy deletion]] nominations made by this user using [[WP:TW|Twinkle]]'s CSD module.\n\n" +
-			'If you no longer wish to keep this log, you can turn it off using the [[Wikipedia:Twinkle/Preferences|preferences panel]], and ' +
-			'nominate this page for speedy deletion under [[WP:CSD#U1|CSD U1]].' +
-			(Morebits.userIsSysop ? '\n\nThis log does not track outright speedy deletions made using Twinkle.' : '');
+			'{{Notice|style=announce|text=即時削除の際、このページへのリンクは考慮する必要はありません。}}\n' +
+			'<!-- このテンプレートは、[[Wikipedia:即時削除の方針#リダイレクト]]にリンク元がある場合は削除しないよう定めてあるため、このページがリンクしていることによる影響を避けるためのものです。 -->\n\n' +
+			'このページは[[H:TW|Twinkle]]のCSDモジュールによって生成されており、[[WP:CSD|即時削除]]に指定したページを記録しています。\n\n' +
+			'記録をこれ以上保持したくない場合、[[利用者:Syunsyunminmin/Twinkle/Preferences|設定パネル]]を使用してこの機能をオフにした後、' +
+			'[[WP:CSD#U1|CSD U1]]でこのページの即時削除を依頼することが出来ます。' +
+			(Morebits.userIsSysop ? '\n\nこのログは、Twinkleを使用して行われた即時削除を完全に追跡してはいません。' : '');
 
 		var formatParamLog = function(normalize, csdparam, input) {
-			if (normalize === 'F5' && csdparam === 'replacement') {
-				input = '[[:' + input + ']]';
-			}
 			return ' {' + normalize + ' ' + csdparam + ': ' + input + '}';
 		};
 
 		var extraInfo = '';
 
 		// If a logged file is deleted but exists on commons, the wikilink will be blue, so provide a link to the log
-		var fileLogLink = ' ([{{fullurl:Special:Log|page=' + mw.util.wikiUrlencode(mw.config.get('wgPageName')) + '}} log])';
+		var fileLogLink = ' ([{{fullurl:Special:Log|page=' + mw.util.wikiUrlencode(mw.config.get('wgPageName')) + '}} 記録])';
 
-		var appendText = '# [[:' + Morebits.pageNameNorm + ']]' + fileLogLink + ': DI [[WP:CSD#' + params.normalized.toUpperCase() + '|CSD ' + params.normalized.toUpperCase() + ']] ({{tl|di-' + params.templatename + '}})';
+		var appendText = '# [[:' + Morebits.pageNameNorm + ']]' + fileLogLink + ': DI [[WP:CSD#' + params.normalized.toUpperCase() + '|CSD ' + params.normalized.toUpperCase() + ']] ({{tl|' + params.templatename + '}})';
 
 		['reason', 'replacement', 'source'].forEach(function(item) {
 			if (params[item]) {
@@ -342,14 +204,14 @@ Twinkle.image.callbacks = {
 		});
 
 		if (extraInfo) {
-			appendText += '; additional information:' + extraInfo;
+			appendText += '; 追加情報:' + extraInfo;
 		}
 		if (initialContrib) {
-			appendText += '; notified {{user|1=' + initialContrib + '}}';
+			appendText += '; {{user|1=' + initialContrib + '}}に通知';
 		}
 		appendText += ' ~~~~~\n';
 
-		var editsummary = 'Logging speedy deletion nomination of [[:' + Morebits.pageNameNorm + ']].';
+		var editsummary = '[[:' + Morebits.pageNameNorm + ']]の即時削除への指定を記録' + Twinkle.summaryAd;
 
 		usl.changeTags = Twinkle.changeTags;
 		usl.log(appendText, editsummary);
